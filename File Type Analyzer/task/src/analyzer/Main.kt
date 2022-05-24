040d30b93357aae7153dad0b4cc5822f3504397c
Krishna Kumar Mandal <krishiway@gmail.com>
2022-05-24 23:15:37 +0200

    Stage 2 done

--- File Type Analyzer/task/src/analyzer/Main.kt
+++ File Type Analyzer/task/src/analyzer/Main.kt
@@ -5,29 +5,113 @@ import java.io.File
 const val ZERO = 0
 const val ONE = 1
 const val TWO = 2
+const val THREE = 3
+const val FOUR = 4
 const val UNKNOWN = "Unknown file type"
 
+enum class ALGORITHM {
+    NAIVE,
+    KMP
+}
+data class Option(
+    val algo: ALGORITHM,
+    val fileName: String,
+    val filePattern: String,
+    val resultString: String
+)
 fun main(args: Array<String>) {
-    if (args.size < 3) {
+
+    val option = parseArgument(args = args)
+    val start = System.nanoTime()
+    val result = findFileType(option)
+    val elapsed = System.nanoTime() - start
+    println(result)
+    println("It took $elapsed seconds")
+}
+
+fun parseArgument(args: Array<String>): Option {
+    if (args.size < FOUR) {
         throw Exception("Wrong number of Input")
     }
 
-    val fileName = args[ZERO]
-    val filePattern = args[ONE]
-    val resultString = args[TWO]
-
-    println(findFileType(fileName, filePattern, resultString))
+    return Option(
+        ALGORITHM.valueOf(args[ZERO].substring(TWO).uppercase()),
+        args[ONE],
+        args[TWO],
+        args[THREE]
+    )
 }
 
-fun findFileType(fileName: String, filePattern: String, resultString: String): String {
-    val file = File(fileName)
+fun naiveSearch(option: Option): String {
+    val file = File(option.fileName)
     var found = false
-    file.readLines().forEach() {
-        if (it.contains(filePattern)) {
-            found = true
+    file.readLines().forEach() { if (it.contains(option.filePattern)) found = true }
+
+    return if (found) option.resultString else UNKNOWN
+}
+
+fun kmpSearch(option: Option): String {
+    val file = File(option.fileName)
+    val text = file.readText()
+
+    val textLength = text.length
+    val pattern = option.filePattern.toCharArray()
+    val patternLength = pattern.size
+    val prefixFunction = MutableList(patternLength) {0}
+    computePrefixFunction(pattern, prefixFunction)
+
+    var textIndex = 0
+    var patternIndex = 0
+
+    while (textIndex < textLength) {
+        if (pattern[patternIndex] == text[textIndex]) {
+            ++textIndex
+            ++patternIndex
         }
+
+        if (patternIndex == patternLength) {
+            return option.resultString
+        } else if (textIndex < textLength && pattern[patternIndex] != text[textIndex]) {
+            if (patternIndex != 0) {
+                patternIndex = prefixFunction[patternIndex - 1]
+            } else {
+                ++textIndex
+            }
+        }
+
     }
 
-    return if (found) resultString else UNKNOWN
+    return UNKNOWN
 }
 
+fun computePrefixFunction(filePattern: CharArray, prefixFunction: MutableList<Int>) {
+
+    // Length of previous prefix suffix
+    var length = 0
+    val patternLength = filePattern.size
+    var index = 1
+    // First prefix function is always 0
+    prefixFunction[ZERO] = ZERO
+
+    while (index < patternLength) {
+        if (filePattern[index] == filePattern[length]) {
+            ++length
+            prefixFunction[index] = length
+            ++index
+        } else {
+            if (length != 0) {
+                length = prefixFunction[length - 1]
+            } else {
+                prefixFunction[index] = ZERO
+                ++index
+            }
+        }
+    }
+}
+
+fun findFileType(option: Option): String {
+    return when (option.algo) {
+        ALGORITHM.NAIVE -> naiveSearch(option)
+        ALGORITHM.KMP -> kmpSearch(option)
+    }
+}
